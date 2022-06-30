a60019ebfe9547852c01d0ed22c3cc7d19ccb995
Jigar-mochi <jigar102024@gmail.com>
2022-06-30 14:05:49 +0530

    Added top scroll bar

---
+++ src/components/extrass.js
@@ -0,0 +1,32 @@
+// handleprevious = async () => {
+//     this.setState({
+//         page: this.state.page - 1
+//     })
+//     this.update()
+
+// }
+// handlenext = async () => {
+//     this.setState({
+//         page: this.state.page + 1
+//     })
+//     this.update()
+// }
+{/* <div className='d-flex justify-content-between'>
+                <button type="button" disabled={this.state.page <= 1} onClick={this.handleprevious} className="btn btn-primary">&#8592; Privious</button>
+                <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pagesize)} onClick={this.handlenext} className="btn btn-primary">Next &#8594;</button>
+            </div> */}
+{/* <form className="d-flex" role="search">
+                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                                <button className="btn btn-outline-success" type="submit">Search</button>
+                            </form> */}
+{/* <li className="nav-item dropdown">
+                                    <a className="nav-a dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Dropdown
+                                    </a>
+                                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <li><a className="dropdown-item" href="/">cricket</a></li>
+                                        <li><a className="dropdown-item" href="/">hockey</a></li>
+                                        <li><hr className="dropdown-divider" /></li>
+                                        <li><a className="dropdown-item" href="/">Something else here</a></li>
+                                    </ul>
+                                </li> */}                            
